bef4693c4fe66961267bcdf40dc3b1d00d7c4632
Valmir silva <valmirdasilva1999@gmail.com>
2021-02-12 15:55:31 -0300

    Primeira 6 aulas

--- script.js
+++
@@ -1,7 +0,0 @@
-//NESTES COMANDOS NÃO É OBRIGATÓRIO UTILIZAR window.
-// Meu primeiro alerta, aparece um popup antes de carregar todo o conteúdo da página.
-window.alert('Minha primeira mensagem!');
-// Este comando é uma segunda confirmação do popup que ainda impede do conteúdo da página carregar.
-window.confirm('Está gostando de JS?');
-// Aparece um popup com um Input para preencher
-window.prompt('Qual é seu nome?');
