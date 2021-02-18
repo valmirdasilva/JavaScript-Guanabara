31c12a928d63496952521cb2c761d235837ec1ab
Valmir silva <valmirdasilva1999@gmail.com>
2021-02-18 09:28:23 -0300

    Aulas guanabara exe 8/9

---
+++ exercicio-aula-11/exercicio8.js
@@ -0,0 +1,8 @@
+var vel = 55
+console.log(`A velocidade do carango é ${vel}KM/velocidade`)
+    if(vel > 60) { //ISTO É CONSIDERADO UM CONDIÇÃO SIMPLES
+        console.log(`Você ultrapassou a velocidade xiru MULTADO`)
+    }
+    else{
+        console.log(`Está velocidade é ideal continue assim!!`)
+    }
---
+++ exercicio-aula-11/exercicio9.js
@@ -0,0 +1,7 @@
+let pais = 'EUA'
+console.log(`Você está vivendo no ${pais}`)
+    if (pais == 'Brasil'){
+        console.log ('você é brasileiro')
+    } else {
+        console.log ('você é estrangeiro')
+    };
