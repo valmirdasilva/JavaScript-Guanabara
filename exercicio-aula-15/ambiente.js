965fa7ddfcf1dced923ee95fc255d186c6095385
Valmir silva <valmirdasilva1999@gmail.com>
2021-03-08 13:41:50 -0300

    Exercicios funções

--- exercicio-aula-15/ambiente.js
+++ exercicio-aula-15/ambiente.js
@@ -1,3 +1,4 @@
-num.sort()
 let num = [10,2,8,9,5]
+num.sort()
+
 console.log(num)
---
+++ exercicio-aula-16/função01.js
@@ -0,0 +1,9 @@
+let res = document.getElementById('res')
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+    console.log(parimpar(10))
+}
